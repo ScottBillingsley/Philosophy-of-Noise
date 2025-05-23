85796a39d7fc2d7fd854906cef3ec9689f1112eb
ScottBillingsley <61607063+ScottBillingsley@users.noreply.github.com>
2025-05-23 06:04:20 -0500

    Update Tone.h

--- AVRduino EDDIE/Program Files/Tone_Simple/Tone.h
+++ AVRduino EDDIE/Program Files/Tone_Simple/Tone.h
@@ -12,6 +12,33 @@
 #define PB6 PIN6_bm
 #define PB7 PIN7_bm
 
+/*
+ *    Tone(uint8_t _pin)            
+ *              Decalres an instance of Tone with pin 
+ *    void init()                   
+ *              Resets the Tone counter to zero
+ *    void update()                 
+ *              Updates the Tone counter and pin state
+ *    void pwm_update()             
+ *              Updates the Tone pwm counter and pin state
+ *    void set_duty(uint8_t duty)   
+ *              Sets the duty cycle of the PWM counter
+ *    void set_compare(uint16_t compare_count)  
+ *              Sets the compare count of the wave
+ *    void pwm_set_compare(uint16_t compare_count)
+ *              Sets the compare count of the PWM wave
+ *    void on()
+ *              Sets the output pin as OUTPUT
+ *    void off()
+ *              Sets the output pin as INPUT
+ *    uint16_t calculate_count(float freq, uint16_t rate)
+ *              Returns the compare count for a given frequency
+ *    uint16_t pwm_calculate_count(float freq, uint16_t rate)          
+ *              Return the pwm compare count for a given frequency
+ *    bool check()          
+ *              Checks if the counter equals the compare count
+ */
+
 class Tone {
 
   private:
